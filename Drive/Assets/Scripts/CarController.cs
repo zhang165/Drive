fff55b93647c47ac4401333a5b1985c0a54ff808
zhang165 <zhang165@gmail.com>
2016-01-25 21:43:24 -0800

    Changed player and car controller

--- Drive/Assets/Scripts/CarController.cs
+++ Drive/Assets/Scripts/CarController.cs
@@ -2,7 +2,8 @@
 using System.Collections;
 using System;
 
-public class CarController : MonoBehaviour {
+public class CarController : MonoBehaviour
+{
     private float speed;
     public float maxSpeed;
     public float backSpeed;
@@ -15,30 +16,38 @@ public class CarController : MonoBehaviour {
     float moveSpeed;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         rb = GetComponent<Rigidbody>();
         rb.centerOfMass += new Vector3(0, 0, 1);
     }
-	
-	// Update is called once per frame
-	void FixedUpdate () {
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
         Vector3 move = new Vector3(0, 0, z);
         if (rb.IsSleeping()) speed = 0; // set acceleration
-
         speed += acceleration;
         if (speed > maxSpeed) speed = maxSpeed;
-        if (Input.GetKey(KeyCode.W)){
-            transform.Translate(Vector3.forward*speed);
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * speed);
         }
         if (Input.GetKey(KeyCode.S))
         {
-            transform.Translate(-Vector3.forward*backSpeed);
+            transform.Translate(-Vector3.forward * backSpeed);
+            speed = 0;
         }
 
-        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.W)) { transform.Rotate(new Vector3(0, x, 0) * turnSpeed);
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.W))
+        {
+            transform.Rotate(new Vector3(0, x, 0) * turnSpeed);
         }
-        
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        speed = 0;
     }
 }
