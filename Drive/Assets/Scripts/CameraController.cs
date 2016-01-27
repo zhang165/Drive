77df1ff1b5618813f18fa11b2fcf2e3eb6616880
zhang165 <zhang165@gmail.com>
2016-01-27 08:17:12 -0800

    Camera toggling

--- Drive/Assets/Scripts/CameraController.cs
+++ Drive/Assets/Scripts/CameraController.cs
@@ -3,15 +3,34 @@ using System.Collections;
 
 public class CameraController : MonoBehaviour {
 
+	public Camera camera1;
+	public Camera camera2;
+	public bool active;
     public GameObject player;
+
     private Vector3 offset;
 	// Use this for initialization
 	void Start () {
-        offset = transform.position - player.transform.position;
+		offset = transform.position - player.transform.position;
+		camera1.gameObject.SetActive (true);
+		camera2.gameObject.SetActive (false);
+		active = true;
 	}
-	
+	void Update(){
+		if (Input.GetKeyDown (KeyCode.C)) {
+			if (active) {
+				camera2.gameObject.SetActive (true);
+				camera1.gameObject.SetActive (false);
+				active = false;
+			} else {
+				camera1.gameObject.SetActive (true);
+				camera2.gameObject.SetActive (false);
+				active = true;
+			}	
+		}
+	}
 	// Update is called once per frame
 	void LateUpdate () {
-        transform.position = player.transform.position + offset;
+				transform.position = player.transform.position + offset;
 	}
 }
