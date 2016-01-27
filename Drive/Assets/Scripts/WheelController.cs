2a1e90f0e60e773147b2d580441ef5d9a7b3297f
zhang165 <zhang165@gmail.com>
2016-01-26 19:16:30 -0800

    Fixed for github

---
+++ Drive/Assets/Scripts/WheelController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WheelController : MonoBehaviour {
+    
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(5, 0, 0) * Time.deltaTime);
+    }
+}
