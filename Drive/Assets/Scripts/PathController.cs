1ee48739d29dd4d3ac3fee36c4f6a9d0b823be02
zhang165 <zhang165@gmail.com>
2016-01-31 00:56:57 -0800

    Minor update: Change to Player color

--- Drive/Assets/Scripts/PathController.cs
+++ Drive/Assets/Scripts/PathController.cs
@@ -2,20 +2,15 @@
 using System.Collections;
 
 public class PathController : MonoBehaviour {
-	Vector3 newPosition;
 
-	void Start(){
-		newPosition = transform.position;
-	}
+    Grid grid;
+	// Use this for initialization
+    void Awake() {
+        grid = GetComponent<Grid>();
+    }
 
-	void Update () {
-		if (Input.GetMouseButtonDown (0)) { // if we click, we want to move an object
-			RaycastHit hit;
-			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-			if (Physics.Raycast (ray, out hit)) {  // TODO: fix bug with placing object on edge
-				newPosition = new Vector3(hit.point.x, 0, hit.point.z) + new Vector3(0,1.2f,0); // zero on hit.point.y so we cannot place target on top of buildings
-				transform.position = newPosition;
-			}
-		}
-	}
+	void FindPath(Vector3 startPos, Vector3 targetPos) {
+        Node startNode = grid.GetNodeFromWorldPoint(startPos);
+        Node targetNode = grid.GetNodeFromWorldPoint(targetPos);
+    }
 }
---
+++ Drive/Assets/Scripts/TargetController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetController : MonoBehaviour {
+	Vector3 newPosition;
+
+	void Start(){
+		newPosition = transform.position;
+	}
+
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) { // if we click, we want to move an object
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if (Physics.Raycast (ray, out hit)) {  // TODO: fix bug with placing object on edge
+				newPosition = new Vector3(hit.point.x, 0, hit.point.z) + new Vector3(0,1.2f,0); // zero on hit.point.y so we cannot place target on top of buildings
+				transform.position = newPosition;
+			}
+		}
+	}
+}
